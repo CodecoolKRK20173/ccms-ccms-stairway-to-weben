0d5fe09ecdbada4f0b4ce8ed4e5ebc3f73a1bb1c
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2018-11-12 21:48:14 +0100

    refactor RegularEmployeeView

--- src/main/java/com/codecool/view/RegularEmployeeView.java
+++ src/main/java/com/codecool/view/RegularEmployeeView.java
@@ -2,12 +2,17 @@ package com.codecool.view;
 
 public class RegularEmployeeView {
 
-    public void printRegularEmployeeMenu(){
-        System.out.println("Welcome student!");
+    public void printMenu(){
+        System.out.println("Welcome!");
         System.out.println("Please choose and option: ");
         System.out.println("1. View list of students with contacts");
         System.out.println("2. Check coffee status");
         System.out.println("3. Check office supplies");
-        System.out.println();
+        System.out.println("0. Log out");
     }
+
+    public void printUnderConstruction(){
+        System.out.println("Under Construction.");
+    }
+
 }
