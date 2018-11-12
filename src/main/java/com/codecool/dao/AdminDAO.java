c6ee4b33f8fc681515d7fb75e8daeef038185a5c
Trauke <piotrekenglert@gmail.com>
2018-11-12 22:28:00 +0100

    add getAdmin

--- src/main/java/com/codecool/dao/AdminDAO.java
+++ src/main/java/com/codecool/dao/AdminDAO.java
@@ -2,5 +2,5 @@ package com.codecool.dao;
 import com.codecool.model.*;
 public interface AdminDAO {
     Admin getAdmin();
-    void updateAdmin(Admin admin);
+//    void updateAdmin(Admin admin);
 }
