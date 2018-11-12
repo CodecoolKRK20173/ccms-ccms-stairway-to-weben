84be10233a1840a65cdf91c1efa4cbaac89cee9c
maciejPaluszkiewicz <maciej.j.paluszkiewicz@gmail.com>
2018-11-12 13:02:50 +0100

    refactoring surename

--- src/main/java/com/codecool/view/RegularEmployeeView.java
+++ src/main/java/com/codecool/view/RegularEmployeeView.java
@@ -3,6 +3,11 @@ package com.codecool.view;
 public class RegularEmployeeView {
 
     public void printRegularEmployeeMenu(){
+        System.out.println("Welcome student!");
+        System.out.println("Please choose and option: ");
+        System.out.println("1. View list of students with contacts");
+        System.out.println("2. Check coffee status");
+        System.out.println("3. Check office supplies");
         System.out.println();
     }
 }
